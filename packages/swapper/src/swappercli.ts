7bbd4214d941f7c13889cb5083ba952dd5abb51e
DaoDev <88341957+DaoDev44@users.noreply.github.com>
2021-10-22 14:15:12 -0600

    small fixes to swapcli

--- packages/swapper/src/swappercli.ts
+++ packages/swapper/src/swappercli.ts
@@ -83,6 +83,7 @@ const main = async (): Promise<void> => {
   }
   if (!buyAsset) {
     console.error(`No asset ${buySymbol} found in asset service`)
+    return
   }
 
   // Swapper Deps
@@ -107,7 +108,13 @@ const main = async (): Promise<void> => {
   const sellAmountBase = toBaseUnit(sellAmount, sellAsset.precision)
 
   const quote = await swapper.buildQuoteTx({
-    input: { sellAsset, buyAsset, sellAmount: sellAmountBase },
+    input: {
+      sellAsset,
+      buyAsset,
+      sellAmount: sellAmountBase,
+      sellAssetAccountId: '0',
+      buyAssetAccountId: '0'
+    },
     wallet
   })
 
